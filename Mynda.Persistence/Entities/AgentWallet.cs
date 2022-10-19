35668ea00d4a096bd8603269d6c6c68136fe229b
Joseph Nma <Josgolf@yahoo.ca>
2022-10-19 10:28:14 +0000

    Deleted AgentWallet.cs

--- Mynda.Persistence/Entities/AgentWallet.cs
+++
@@ -1,8 +0,0 @@
-namespace Mynda.Persistence.Entities
-{
-    public class AgentWallet
-    {
-        public string WalletBalance { get; set; }
-        public string WalletNumber { get; set; }
-    }
-}
