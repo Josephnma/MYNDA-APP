ebf2756fed5f163040bcfcaf9b1409d790c88fd2
Joseph Nma <Josgolf@yahoo.ca>
2022-10-19 10:28:06 +0000

    Deleted AgentSavedJobsFooter.cs

--- Mynda.Persistence/Entities/AgentSavedJobsFooter.cs
+++
@@ -1,9 +0,0 @@
-namespace Mynda.Persistence.Entities
-{
-    public class AgentSavedJobsFooter
-    {
-        public List<string>? SavedJobs { get; set; }
-        
-        public List<string>? SavedMyndas { get; set; }
-    }
-}
